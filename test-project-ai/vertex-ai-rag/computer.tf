f806431d0c54c0720acf81626eba415234704d39
Randy Guo <randyzwguo@qq.com>
2024-09-22 16:04:29 +0800

    Create computer.tf

---
+++ test-project-ai/vertex-ai-rag/computer.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "default" {
+  name = "default"
+  machine_type = "e2-micro"
+  network_interface {
+    network = "default"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-focal-v20220712"
+    }
+  }
+  # Some changes require full VM restarts
+  # consider disabling this flag in production
+  #   depending on your needs
+  allow_stopping_for_update = true
+}
